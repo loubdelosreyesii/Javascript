a1d3fbd31249c35f9d817f1c2cfba7367691210b
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2023-08-17 14:43:43 +0800

    Lesson 07: Swtich Statements Done

---
+++ Javascript 07. Switch Statements/js/main.js
@@ -0,0 +1,17 @@
+
+
+switch(1)
+{
+    case 1:
+        console.log(1);
+        break;
+
+    case 2:
+        console.log(2);
+        break;
+    case 3:
+        console.log(3);
+        break;
+    default:
+        console.log("No match");
+}
