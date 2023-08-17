aeb2bf8934b2e39f50946066d134dc2da4336b9b
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2023-08-17 12:09:06 +0800

    Recommit start of Lesson 06

--- js/main.js
+++
@@ -1,7 +0,0 @@
-//Write Javascript Code that will return a random letter from your name.
-
-const myName = "LouB.delosReyesII";
-
-console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
-
-console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
