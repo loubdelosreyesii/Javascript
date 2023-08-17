2c68f2db50d27847f09b7578a9b4ef7c229b5936
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2023-08-17 10:45:03 +0800

    Lesson 02 : Done

---
+++ Javascript 02. Strings/js/main.js
@@ -0,0 +1,25 @@
+const myVariable = "Mathematics";
+
+console.log(myVariable.length);
+
+console.log(myVariable.charAt(6));
+
+console.log(myVariable.indexOf("at"));
+
+console.log(myVariable.lastIndexOf("at"));
+
+console.log(myVariable.slice(5));
+
+console.log(myVariable.slice(5,8));
+
+console.log(myVariable.slice(5,6));
+
+console.log(myVariable.toUpperCase());
+
+console.log(myVariable.toLowerCase());
+
+console.log(myVariable.includes("mat"));
+
+console.log(myVariable.split("e"));
+
+console.log(myVariable.split(""));
