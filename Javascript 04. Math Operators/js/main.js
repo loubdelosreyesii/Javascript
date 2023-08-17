bd84540ea95a0458928e60b8dd27a51802dc6160
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2023-08-17 11:33:06 +0800

    Lesson 04 : Done

---
+++ Javascript 04. Math Operators/js/main.js
@@ -0,0 +1,28 @@
+//Math Methods and Properties
+
+console.log(Math.PI);
+
+console.log(Math.trunc(Math.PI));
+
+console.log(Math.round(3.5));
+
+// round up
+console.log(Math.ceil(Math.PI));
+
+// round down
+console.log(Math.floor(Math.PI))
+
+console.log(Math.pow(2,4));
+
+console.log(Math.min(1,2,3,1,0.2));
+
+console.log(Math.max(1,11,3,1,0.2));
+
+// From 0 to 1
+console.log(Math.random());
+
+console.log(Math.floor(Math.random()*10+1));
+console.log(Math.floor(Math.random()*10+1));
+console.log(Math.floor(Math.random()*10+1));
+console.log(Math.floor(Math.random()*10+1));
+console.log(Math.floor(Math.random()*10+1));
