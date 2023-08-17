53f0dd975388cc61d12aa733619e35cdf618d79a
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2023-08-17 15:00:43 +0800

    Lesson 09: Done

---
+++ Javascript 09. User Input/js/main.js
@@ -0,0 +1,14 @@
+// alert("Hello world!");
+
+// let myBoolean = confirm("Ok===True\nCancel===False");
+
+// console.log(myBoolean);
+
+// if onkeydown, return the value
+// if cancelled, null value will return;
+let name = prompt("Please enter your name.");
+
+// console.log(name);
+
+console.log(name ?? "You did not enter your name.");
+
