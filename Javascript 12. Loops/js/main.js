64ee7ea7ffbaa8d923d62fd58b7d4895c7b22f7c
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2023-08-17 16:00:22 +0800

    Lesson 12 : Loops :done

---
+++ Javascript 12. Loops/js/main.js
@@ -0,0 +1,21 @@
+let myNumber = 50;
+
+// while(myNumber <50){
+//     console.log(myNumber);
+//     // myNumber++;
+//     myNumber+=2;
+// }
+
+// do{
+//     // myNumber++;
+//     myNumber+=1;
+//     console.log(myNumber);
+// }
+// while(myNumber <=50);
+let i=0;
+//for(let i=0; i<=10;){ //allowed
+//for(; i<=10;i++){  //alowed
+for(; i<=10;){
+    console.log(i);
+    i++;
+}
